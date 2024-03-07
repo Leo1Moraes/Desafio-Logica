39240bb95ef2df59c8ad53a407850e8387eab72f
Leo1Moraes <leomoraes75@outlook.com>
2024-03-07 19:32:11 -0300

    teste

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-// Status Herói
-let heroi = "Poucas Trancas"
-let pontosXP = 956
+// Status Heróis
+let Heroi = "Thor Deus do Trovão"
+let pontosXP = 958
 
-// Pontos de XP do herói
-let nivelHeroi = 
+// Pontos de XP do Herói
+let nivelHeroi;
 
-if (pontosXP <= 1000){
-    nivelHeroi = "Ferro"
-} else if (pontosXP >= 1001 && pontosXP <= 2000){
+if (pontosXP < 1000) {
+    nivelHeroi = "Ferro";
+} else if (pontosXP >= 1001 && <= 2000){
     nivelHeroi = "Bronze"
+} else if (pontosXP >= 2001 && <= 5000){
+    nivelHeroi = "Prata"
+} else if (pontosXP >= 5001 && <= 7000){
+    nivelHeroi = "Ouro"
+} else if (pontosXP >= 7001 && <= 8000){
+    nivelHeroi = "Platina"
+} else if (pontosXP >= 8001 && <= 9000){
+    nivelHeroi = "Ascendente"
+} else if (pontosXP >= 9001 && <= 10000){
+    nivelHeroi = "Imortal"
+} else if (pontosXP >= 10001){
+    nivelHeroi = "Radiante"
 }
 
 console.log("O Herói de nome " + heroi , "está no nível " + nivelHeroi)
