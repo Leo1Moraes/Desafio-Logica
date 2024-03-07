3e10ba66e941d08c857505109f077b2f493be8fe
Leo1Moraes <leomoraes75@outlook.com>
2024-03-07 19:12:36 -0300

    testando

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 // Status Herói
-let heroi = "Poucas Trancas";
-let pontosXP = 956;
+let heroi = "Poucas Trancas"
+let pontosXP = 956
 
 // Pontos de XP do herói
 let nivelHeroi = 
 
-switch (pontosXP){
-    case pontosXP >= 0 && pontosXP <= 1000
-    nivelHeroi = "Ferro";
-    break;
-    case pontosXP >= 1001 && pontosXP<= 2000
-    nivelHeroi = "Bronze";
-    break;
+if (pontosXP <= 1000){
+    nivelHeroi = "Ferro"
+} else if (pontosXP >= 1001 && pontosXP <= 2000){
+    nivelHeroi = "Bronze"
 }
+
+console.log("O Herói de nome " + heroi , "está no nível " + nivelHeroi)
