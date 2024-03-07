5705b4abf82b207b717c1a4956d0fe860904ccff
Leo1Moraes <leomoraes75@outlook.com>
2024-03-07 19:37:32 -0300

    teste

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ let nivelHeroi;
 
 if (pontosXP < 1000) {
     nivelHeroi = "Ferro";
-} else if (pontosXP >= 1001 && <= 2000){
+} else if (pontosXP >= 1001 && pontosXP <= 2000){
     nivelHeroi = "Bronze"
-} else if (pontosXP >= 2001 && <= 5000){
+} else if (pontosXP >= 2001 && pontosXP <= 5000){
     nivelHeroi = "Prata"
-} else if (pontosXP >= 5001 && <= 7000){
+} else if (pontosXP >= 5001 && pontosXP <= 7000){
     nivelHeroi = "Ouro"
-} else if (pontosXP >= 7001 && <= 8000){
+} else if (pontosXP >= 7001 && pontosXP <= 8000){
     nivelHeroi = "Platina"
-} else if (pontosXP >= 8001 && <= 9000){
+} else if (pontosXP >= 8001 && pontosXP <= 9000){
     nivelHeroi = "Ascendente"
-} else if (pontosXP >= 9001 && <= 10000){
+} else if (pontosXP >= 9001 && pontosXP <= 10000){
     nivelHeroi = "Imortal"
 } else if (pontosXP >= 10001){
     nivelHeroi = "Radiante"
